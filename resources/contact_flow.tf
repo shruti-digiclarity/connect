42f166ff54e05c4f2135e5a9a668ead53c992b06
prashant-khunt-cloverhealth <prashant.khunt@vendors.cloverhealth.com>
2025-08-01 18:51:31 +0530

    update flow

--- resources/contact_flow.tf
+++ resources/contact_flow.tf
@@ -11,6 +11,7 @@ locals {
       description = "ch_voice_mail_flow"
       tags        = local.tags
     }
+
     "ch_telesales_main_flow" = {
       content = templatefile(
         "${path.module}/contact-flows/ch_telesales_main_flow.json.tftpl",
