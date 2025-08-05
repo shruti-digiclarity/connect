1ef93f725878cc6a5be48ce8cb932db04298b24c
prashant-khunt-cloverhealth <prashant.khunt@vendors.cloverhealth.com>
2025-08-05 10:55:02 +0530

    remove telesales resources

--- resources/hierarchy_structure.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  user_hierarchy_structure = {
-    level_one   = "lob"
-    level_two   = "admin"
-    level_three = "supervisor"
-    level_four  = "qa or team lead"
-    level_five  = "agent"
-  }
-}
