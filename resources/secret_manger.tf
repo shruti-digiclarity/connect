b573fffd88580821abc3512b8488aa9d6d3fd84b
prashant-khunt-cloverhealth <prashant.khunt@vendors.cloverhealth.com>
2025-08-11 15:25:49 +0530

    create secret manager

---
+++ resources/secret_manger.tf
@@ -0,0 +1,29 @@
+module "connect_lead_generation_secret" {
+  source = "git@github.com:CloverHealth/ccaas-terraform-modules-wrapper.git//terraform-aws-secrets-manager-wrapper?ref=master"
+
+  items = {
+    connect_lead_generation = {
+      name                    = "aws-connect-lead-generation"
+      description             = "Secret for connect lead generation"
+      secret_string           = jsonencode({})
+      tags                    = local.tags
+      create                  = true
+      recovery_window_in_days = 30
+    }
+  }
+}
+
+module "connect_campaign_attribution_secret" {
+  source = "git@github.com:CloverHealth/ccaas-terraform-modules-wrapper.git//terraform-aws-secrets-manager-wrapper?ref=master"
+
+  items = {
+    connect_lead_generation = {
+      name                    = "aws-connect-campaign-attribution"
+      description             = "Secret for connect campaign attribution"
+      secret_string           = jsonencode({})
+      tags                    = local.tags
+      create                  = true
+      recovery_window_in_days = 30
+    }
+  }
+}
