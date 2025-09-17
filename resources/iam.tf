92bd3da89bb6186e62edb903d20b2fb55a1606e8
prashant-khunt-cloverhealth <prashant.khunt@vendors.cloverhealth.com>
2025-09-17 17:58:36 +0530

    attach policy with user

--- resources/iam.tf
+++ resources/iam.tf
@@ -91,7 +91,5 @@ module "iam_user" {
   create_user   = true
   name          = format("%s-iam-vm-s3-presigned-%s-%s", var.company_prefix, local.region_prefix, var.env)
   force_destroy = "true"
-  policy_arns = {
-    s3_access = module.iam_user_policy.arn
-  }
+  policy_arns   = [module.iam_user_policy.arn]
 }
