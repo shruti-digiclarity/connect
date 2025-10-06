bc55f26a9994845cb35fab2d2ea5c9ccaa706296
prashant-khunt-cloverhealth <prashant.khunt@vendors.cloverhealth.com>
2025-10-06 11:02:29 +0530

    Queue Configurations

--- resources/queues.tf
+++ resources/queues.tf
@@ -6,7 +6,7 @@ locals {
       status                = "ENABLED"
       tags                  = local.tags
     }
-    quick_connect_queue = {
+    ch_quick_connect_queue = {
       description = "Quick Connect Queue"
       hours_of_operation_id = module.amazon_connect.hours_of_operations["ch_constant_hours"].hours_of_operation_id
       statement = "ENABLED"
